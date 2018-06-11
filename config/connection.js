9108dbc3b5bca43e9a27ddbc03df6eb0a13ecd59
Elizabeth Rogers <elle.eels@gmail.com>
2018-06-11 00:24:47 -0700

    jaws connection

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  database : 'burgers_db',
-  port: '8889'
+  host     : 'qzkp8ry756433yd4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user     : 'uyexy8ia4ukmm6ot',
+  password : 'lsnt6mwgyqxzacoe',
+  database : 'u9mk6xdp7zu45v86',
+  port: '3306'
 });
  
 module.exports = connection
